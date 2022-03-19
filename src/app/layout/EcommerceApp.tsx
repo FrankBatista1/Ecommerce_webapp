bffd367b1ef2904119767306e03c54a08568a4f3
FrankBatista1 <fb53901@gmail.com>
2022-03-18 22:32:43 -0500

    routing added

--- src/app/layout/EcommerceApp.tsx
+++ src/app/layout/EcommerceApp.tsx
@@ -7,6 +7,11 @@ import {
   ThemeProvider,
 } from "@mui/material";
 import { useState } from "react";
+import { Route } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ContactPage from "../../features/contact/ContactPage";
+import AboutPage from "../../features/about/AboutPage";
 
 const EcommerceApp = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -27,7 +32,11 @@ const EcommerceApp = () => {
         <CssBaseline />
         <NavBar darkMode={darkMode} switchDarkMode={swtichDarkMode} />
         <Container>
-          <Catalog />
+          <Route exact path='/' component={HomePage} />
+          <Route exact path="/catalog" component={Catalog} />
+          <Route exact path="/catalog/:id" component={ProductDetails} />
+          <Route exact path="/about" component={AboutPage} />
+          <Route exact path="/contact" component={ContactPage} />
         </Container>
       </ThemeProvider>
     </>
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import EcommerceApp from './app/layout/EcommerceApp'
-import './styles/styles.scss'
+import { BrowserRouter } from "react-router-dom";
+import EcommerceApp from "./app/layout/EcommerceApp";
+import "./styles/styles.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <EcommerceApp />
+    <BrowserRouter>
+      <EcommerceApp />
+    </BrowserRouter>
   </React.StrictMode>,
-
   document.getElementById("root")
 );
